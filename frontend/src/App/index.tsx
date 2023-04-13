d4c4b1ed0dd42fc21f98eab68d3159092d22494e
Minhyeok Park <opensource@pmh.codes>
2023-04-13 15:02:25 +0900

    refactor: remove old frontend

--- frontend/src/App/index.tsx
+++ frontend/src/App/index.tsx
@@ -1,18 +1,6 @@
 import { type FC } from 'react'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
-import { askingRoute } from '../AskingPage'
-import { ingameRouter } from '../IngamePage'
-import { introRoute } from '../IntroPage'
-import { scoreRouter } from '../ScorePage'
-
-const router = createHashRouter([
-  introRoute,
-  askingRoute,
-  ingameRouter,
-  scoreRouter
-])
 
 const App: FC = () =>
-  <RouterProvider router={router} />
+  <></>
 
 export default App
