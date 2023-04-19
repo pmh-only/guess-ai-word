b6ff4b7110b1357cfdf849d7050207acfda943b7
Minhyeok Park <opensource@pmh.codes>
2023-04-19 21:47:41 +0900

    feat: size adjustment

--- frontend/src/BottomTabNavigation/index.tsx
+++ frontend/src/BottomTabNavigation/index.tsx
@@ -3,30 +3,38 @@ import style from './style.module.scss'
 import { FaInfoCircle, FaAward, FaGamepad } from 'react-icons/fa'
 import { useLocation, useNavigate } from 'react-router-dom'
 
+import { motion } from 'framer-motion'
+
 const BottomTabNavigation: FC = () => {
   const location = useLocation()
   const navigate = useNavigate()
 
   return (
     <nav className={style.bottomTab}>
-      <button
+      <motion.button
+        transition={{ duration: 0.2 }}
+        whileTap={{ backgroundColor: 'var(--main-secondary)' }}
         className={location.pathname === '/credits' ? style.enabled : ''}
         onClick={() => { navigate('/credits') }}>
         <FaInfoCircle />
         <label>게임 정보</label>
-      </button>
-      <button
+      </motion.button>
+      <motion.button
+        transition={{ duration: 0.2 }}
+        whileTap={{ backgroundColor: 'var(--main-secondary)' }}
         className={location.pathname === '/' ? style.enabled : ''}
         onClick={() => { navigate('/') }}>
         <FaGamepad />
         <label>홈</label>
-      </button>
-      <button
+      </motion.button>
+      <motion.button
+        transition={{ duration: 0.2 }}
+        whileTap={{ backgroundColor: 'var(--main-secondary)' }}
         className={location.pathname === '/boards' ? style.enabled : ''}
         onClick={() => { navigate('/boards') }}>
         <FaAward />
         <label>리더보드</label>
-      </button>
+      </motion.button>
     </nav>
   )
 }
