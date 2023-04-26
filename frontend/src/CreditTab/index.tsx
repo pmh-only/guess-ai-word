c4b57c8a3569387956fa6a106ba987a7a92056be
Minhyeok Park <opensource@pmh.codes>
2023-04-26 12:03:06 +0900

    feat: improve game info page

--- frontend/src/CreditTab/index.tsx
+++ frontend/src/CreditTab/index.tsx
@@ -7,16 +7,10 @@ const CreditTab: FC = () =>
     <h1>Credit.</h1>
     <p>Guess AI Word</p>
 
-    <h2>Figma UI/UX</h2>
-    <p>Minhyeok Park</p>
-
     <h2>Spring Backend</h2>
     <p>Minhyeok Park</p>
 
     <h2>React.js Frontend</h2>
-    <p>Minhyeok Park</p>
-
-    <h2>Prompt Engineering</h2>
     <p>Minhyeok Park</p>
 
     <h2>Powered by</h2>
@@ -28,8 +22,9 @@ const CreditTab: FC = () =>
     <h2>Source code</h2>
     <p><a target='_blank' href="https://github.com/pmh-only/guess-ai-word" rel="noreferrer">pmh-only/guess-ai-word</a></p>
 
-    <h2>Extra</h2>
+    <h2>Copyright</h2>
     <p>Copyright &copy; 2023. Minhyeok Park.</p>
+
     <p><MdMail /> opensource@pmh.codes</p>
     <p><MdHeartBroken /></p>
   </section>
