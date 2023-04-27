da361aad90da21bdc5ddd5ed5d240b934442defa
Minhyeok Park <opensource@pmh.codes>
2023-04-27 17:14:30 +0900

    feat: change leaderboard page style

--- frontend/src/BoardTab/index.tsx
+++ frontend/src/BoardTab/index.tsx
@@ -2,6 +2,7 @@ import { useState, type FC, useEffect } from 'react'
 
 import style from './style.module.scss'
 import TitleBar from '../TitleBar'
+import { MdArrowForward } from 'react-icons/md'
 
 interface GameDetail {
   dictionaryCategory: string
@@ -22,40 +23,54 @@ interface GameDetail {
 }
 
 const BoardTab: FC = () => {
-  const [isLast, setIsLast] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [lastId, setLastId] = useState(0)
-  const [gameDetail, setGameDetail] = useState<GameDetail[]>([])
+  const [games, setGames] = useState<Map<string, GameDetail[]>>(new Map())
 
-  const fetchGameHistory = async (): Promise<void> => {
-    if (isLast || isLoading) return
-    setIsLoading(true)
-
-    const { last, games } = await fetch('/api/games/getGameHistory', {
+  const fetchGameLeaderboard = (gameType: string): void => {
+    void fetch('/api/games/getLeaderBoard', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        lastId,
-        count: 10
-      })
+      body: JSON.stringify({ gameType })
     }).then(async (res) => await res.json())
-
-    setGameDetail([
-      ...gameDetail,
-      ...games
-    ])
-    setLastId(games[games.length - 1]?.id ?? 0)
-    setIsLast(last)
-    setIsLoading(false)
+      .then(({ games: receivedGames }) => {
+        setGames((games) => {
+          games.set(gameType, receivedGames)
+          return new Map(games)
+        })
+      })
   }
 
   useEffect(() => {
-    void fetchGameHistory()
+    fetchGameLeaderboard('SPEEDRUN')
+    fetchGameLeaderboard('NORMAL')
   }, [])
 
   return (
     <section className={style.container}>
       <TitleBar title="리더보드" isFreepass />
+      <ul className={style.itemList}>
+        <li>
+          <p>난이도</p>
+          <p>스피드런</p>
+        </li>
+        {games.get('SPEEDRUN')?.map((game, i) => (
+          <li key={i}>
+            <p><i>{i + 1}등</i> {game.playerName ?? '익명'}</p>
+            <p><i>{game.finalScore}</i>점</p>
+            <button>자세히 <MdArrowForward /></button>
+          </li>
+        ))}
+        <li>
+          <p>난이도</p>
+          <p>노말</p>
+        </li>
+        {games.get('NORMAL')?.map((game, i) => (
+          <li key={i}>
+            <p><i>{i + 1}등</i> {game.playerName ?? '익명'}</p>
+            <p><i>{game.finalScore}</i>점</p>
+            <button>자세히 <MdArrowForward /></button>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
