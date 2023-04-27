6f33c1482c41703a348ded96912f81393aa184c9
Minhyeok Park <opensource@pmh.codes>
2023-04-27 19:35:14 +0900

    feat: use hash router instead of browser router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,8 @@ import App from './App'
 
 import './colors.scss'
 import './global.scss'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLDivElement)
-  .render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>)
+  .render(<StrictMode><HashRouter><App /></HashRouter></StrictMode>)
